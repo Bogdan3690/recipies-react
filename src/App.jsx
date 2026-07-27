dbf55a916940e7e00efa5256938377f3cea544bc
Bogdan3690 <dblevko@gmail.com>
2026-07-27 13:29:14 -0600

    add icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@
 import './App.css'
 import Recipe from './components/Recipe'
 import recipes from './data/recipes.json'
+import { FaFire} from 'react-icons/fa';
+import { MdTimer } from "react-icons/md";
+import { PiSteps } from "react-icons/pi";
+import { GiKnifeFork } from "react-icons/gi";
+
 
 function App() {
 
@@ -11,7 +16,7 @@ function App() {
     <h1>Menu</h1>
     {recipes && recipes.map((recipe, index) => (
       <li key={index}>
-        <Recipe recipeItem={recipe} />
+        <Recipe recipeItem={recipe} /> <GiKnifeFork />  <FaFire /> <PiSteps /> <MdTimer/>
       </li>
     ))}
     </>
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,8 +1,11 @@
+import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
+
+
 const Recipe = (props) => {
     return(
         <div>
             <img width={400} src={props.recipeItem.image} alt={props.recipeItem.name} />
-            <h3>{props.recipeItem.name}</h3>
+            <h3><MdOutlineDriveFileRenameOutline /> {props.recipeItem.name}</h3>
         </div>
     )
 }
